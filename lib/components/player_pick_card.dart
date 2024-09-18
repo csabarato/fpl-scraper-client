91d8b361c68088e263036cd5d0cbcd28489f4e32
Csaba <csaba.arato@finshape.com>
2024-09-18 08:04:20 +0200

    Create a ListView with ExpansionTiles that contains who picked the players

---
+++ lib/components/player_pick_card.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/material.dart';
+
+class PlayerPickCard extends StatelessWidget {
+  const PlayerPickCard(this.participantName, this.multiplier, {super.key});
+
+  final String participantName;
+  final int multiplier;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60.0,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                  width: 200.0,
+                  child: Text(participantName)),
+
+              Text(getPlayerRoleText(multiplier))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  String getPlayerRoleText(int multiplier) {
+    switch (multiplier) {
+      case 0: return 'Bench';
+      case 1: return 'Starting 11';
+      case 2: return 'Captain';
+      case 3: return 'Triple captain';
+      default: return '';
+    }
+  }
+}
--- lib/screens/picks_screen.dart
+++ lib/screens/picks_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:fpl_scraper_client/components/player_pick_card.dart';
 import 'package:fpl_scraper_client/models/player_model.dart';
 import 'package:fpl_scraper_client/services/participant_service.dart';
 
@@ -9,13 +10,11 @@ class PicksScreen extends StatefulWidget {
 
   final String? leagueId;
 
-
   @override
   State<PicksScreen> createState() => _PicksScreenState();
 }
 
 class _PicksScreenState extends State<PicksScreen> {
-
   late Map<String, dynamic> participantsMap;
   late List<PlayerModel> playerList = [];
 
@@ -30,10 +29,10 @@ class _PicksScreenState extends State<PicksScreen> {
             .then((resp) {
           Map<String, dynamic> json = jsonDecode(utf8.decode(resp.bodyBytes));
           for (Map<String, dynamic> element in json['picks']) {
-            print(element);
             playerList.add(PlayerModel.fromJson(element));
           }
-          //print(playerList);
+          print('loaded ${playerList.length} players');
+          setState(() {});
         });
       });
     }
@@ -41,8 +40,33 @@ class _PicksScreenState extends State<PicksScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Scaffold(
+        body: ListView.builder(
+            itemCount: playerList.length,
+            itemBuilder: (context, playerIndex) {
+              final player = playerList[playerIndex];
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ExpansionTile(
+      
+                  title: Text(player.name),
+                  subtitle: Text('Picked by ${player.numOfPicks} players'),
+                  children: [
+                    SizedBox(
+                      height: player.playerPicks.length * 60.0,
+                      child: ListView.builder(
+                          itemCount: player.playerPicks.length,
+                          itemBuilder: (context, pickIndex) {
+                            return PlayerPickCard(participantsMap[player.playerPicks[pickIndex].participantId], player.playerPicks[pickIndex].multiplier);
+                          }),
+                    ),
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
-
-
 }
